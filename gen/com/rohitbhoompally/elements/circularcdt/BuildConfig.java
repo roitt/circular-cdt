2d22f9a32eb1b379fa8246b1b634e31c1471dc5f
Rohit Bhoompally <bhoomprt@mail.uc.edu>
2014-07-08 16:09:33 -0400

    Initial commit

---
+++ gen/com/rohitbhoompally/elements/circularcdt/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.rohitbhoompally.elements.circularcdt;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
