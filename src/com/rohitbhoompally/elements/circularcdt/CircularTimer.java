439c6ee5473734ddeea218915728f93807e213ba
Rohit Bhoompally <bhoomprt@mail.uc.edu>
2014-07-09 11:51:45 -0400

    Circular timer class.

---
+++ src/com/rohitbhoompally/elements/circularcdt/CircularTimer.java
@@ -0,0 +1,44 @@
+/** Circular Timer
+ *  Written by Rohit
+ *  Jul 8, 2014
+ */
+package com.rohitbhoompally.elements.circularcdt;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CircularTimer extends View {
+
+	// Default values for variables
+	private int rimThickness = 20;
+	private int rimDefaultColor = 0xAEF7F7F7;
+	private int rimFillColor = 0xAFBFFF00;
+	private boolean showMinutes = false;
+	private boolean showHours = false;
+	private boolean rimInnerShadow = false;
+
+	/**
+	 * @param context
+	 * @param attrs
+	 */
+	public CircularTimer(Context _context, AttributeSet _attrs) {
+		super(_context, _attrs);
+		// TODO Auto-generated constructor stub
+		init(_context, _attrs);
+	}
+
+	public void init(Context _context, AttributeSet _attrs) {
+		// Get the properties from resource file
+		if (_context != null && _attrs != null) {
+			TypedArray typedArray = _context.obtainStyledAttributes(_attrs,
+					R.styleable.CircularTimer);
+			rimThickness = (int) typedArray.getDimension(
+					R.styleable.CircularTimer_rimThickness, rimThickness);
+			rimDefaultColor = (int) typedArray.getDimension(
+					R.styleable.CircularTimer_rimDefaultColor, rimDefaultColor);
+			typedArray.recycle();
+		}
+	}
+}
